0f5f672c29351f59e3bc47cbbab84e722dfdbc31
caltimmons <40839829+caltimmons@users.noreply.github.com>
2019-04-30 17:29:36 -0400

    Add files via upload

--- Final_Project_File.py
+++ Final_Project_File.py
@@ -37,6 +37,40 @@ def transcribe(dna):
     return rna_list
 
 
+RNA_CODON_TABLE= {
+    'UUU': 'F',    'CUU': 'L',    'AUU': 'I',    'GUU': 'V',
+    'UUC': 'F',    'CUC': 'L',    'AUC': 'I',    'GUC': 'V',
+    'UUA': 'L',    'CUA': 'L',    'AUA': 'I',    'GUA': 'V',
+    'UUG': 'L',    'CUG': 'L',    'AUG': 'M',    'GUG': 'V',
+    'UCU': 'S',    'CCU': 'P',    'ACU': 'T',    'GCU': 'A',
+    'UCC': 'S',    'CCC': 'P',    'ACC': 'T',    'GCC': 'A',
+    'UCA': 'S',    'CCA': 'P',    'ACA': 'T',    'GCA': 'A',
+    'UCG': 'S',    'CCG': 'P',    'ACG': 'T',    'GCG': 'A',
+    'UAU': 'Y',    'CAU': 'H',    'AAU': 'N',    'GAU': 'D',
+    'UAC': 'Y',    'CAC': 'H',    'AAC': 'N',    'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q',    'AAA': 'K',    'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q',    'AAG': 'K',    'GAG': 'E',
+    'UGU': 'C',    'CGU': 'R',    'AGU': 'S',    'GGU': 'G',
+    'UGC': 'C',    'CGC': 'R',    'AGC': 'S',    'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R',    'AGA': 'R',    'GGA': 'G',
+    'UGG': 'W',    'CGG': 'R',    'AGG': 'R',    'GGG': 'G'
+}
+
+
+def translate(rna_list):
+    protein_list = []
+
+    for x in rna_list:
+        protein = ''
+        for i in range(0, len(x), 3):
+            symbol = RNA_CODON_TABLE[x[i:i+3]]
+            if symbol == 'Stop':
+                break
+            protein += symbol
+        protein_list.append(protein)
+    return protein_list
+
+
 def main():
     # Asks the user for the name of a file for dna data
     data_file = input('Enter the name of the file to use (include file extension): ')
@@ -49,5 +83,8 @@ def main():
     rna_list = transcribe(dna_list)
     print(rna_list)
 
+    # Sends RNA data to be translated
+    protein_list = translate(rna_list)
+    print(protein_list)
 
 main()
